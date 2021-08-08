5a868db483f1795bcd0bad815b0682cbaf9dbba2
himax82 <himax82@gmail.com>
2021-08-08 13:01:21 +0800

    4. Сортировка номера [#221284 #187041]

--- src/ru/job4j/collection/LexSort.java
+++ src/ru/job4j/collection/LexSort.java
@@ -8,11 +8,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftAr = left.split("\\.");
         String[] rightAr = right.split("\\.");
-        for (int i = 0; i < 3; i++) {
-            if (Integer.parseInt(leftAr[i]) != Integer.parseInt(rightAr[i])) {
-                return Integer.compare(Integer.parseInt(leftAr[0]), Integer.parseInt(rightAr[0]));
-            }
-        }
-        return left.compareTo(right);
+        return Integer.compare(Integer.parseInt(leftAr[0]), Integer.parseInt(rightAr[0]));
     }
 }
