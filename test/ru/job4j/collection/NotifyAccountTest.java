18331d6ecd07dad0098053ddbfbc1df20cb65027
himax82 <himax82@gmail.com>
2021-08-15 10:47:07 +0800

    1. Метод .of [#70623]

--- test/ru/job4j/collection/NotifyAccountTest.java
+++ test/ru/job4j/collection/NotifyAccountTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,12 +13,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -27,13 +28,13 @@ public class NotifyAccountTest {
 
     @Test
     public void deleteDuplicate() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
